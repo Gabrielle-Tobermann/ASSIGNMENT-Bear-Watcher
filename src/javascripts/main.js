65dec277e571e1edcc6e563c3b957a2a7eba30a6
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-02-18 20:14:23 -0600

    added things in package.json, create a form, printToDom

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,28 @@
 import '../styles/main.scss';
 
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToPrint;
+};
+
+const createForm = () => {
+  const formString = `<form>
+  <div class="form-group">
+    <label for="bearName">Enter bear name</label>
+    <input type="text" class="form-control" id="bear-name" aria-describedby="bearName">
+  </div>
+  <div class="form-group">
+    <label for="img-url">Image</label>
+    <input type="url" class="form-control" id="exampleInputPassword1">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>`;
+
+  printToDom('#bear-form', formString);
+};
+
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  createForm();
 };
 
 init();
