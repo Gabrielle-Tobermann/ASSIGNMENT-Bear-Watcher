48c17848822a5a5df64e6f976d66a09e62578dd7
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-02-23 15:42:26 -0600

    rearranged modules

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,18 +1,10 @@
 import '../styles/main.scss';
-import {
-  bears, getFormInfo, succeed, attempt
-} from './Helpers/Data/bears';
-import createForm from './Components/createForm';
-import createCards from './Components/createCards';
-import river from './Components/river';
+import initialScreen from './Components/initialScreen';
+import domEvents from './Helpers/domEvents';
 
 const init = () => {
-  createForm();
-  createCards(bears);
-  document.querySelector('form').addEventListener('submit', getFormInfo);
-  document.querySelector('#river-btn').addEventListener('click', river);
-  document.querySelector('#bearCard').addEventListener('click', attempt);
-  document.querySelector('#bearCard').addEventListener('click', succeed);
+  initialScreen();
+  domEvents();
 };
 
 init();
